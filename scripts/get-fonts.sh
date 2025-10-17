dd02122988f1ca430648425070a7db8259bd7781
LamKamhang <lamkamh@zju.edu.cn>
2025-10-17 14:08:14 +0800

    fix font download path

--- scripts/get-fonts.sh
+++ scripts/get-fonts.sh
@@ -1,12 +1,12 @@
 mkdir -p fonts
 pushd fonts
-wget -c https://raw.githubusercontent.com/Haixing-Hu/latex-chinese-fonts/master/english/Serif/TimesNewRoman.ttf
-wget -c https://raw.githubusercontent.com/Haixing-Hu/latex-chinese-fonts/master/english/Serif/TimesNewRomanBold.ttf
-wget -c https://raw.githubusercontent.com/Haixing-Hu/latex-chinese-fonts/master/english/Serif/TimesNewRomanItalic.ttf
-wget -c https://raw.githubusercontent.com/Haixing-Hu/latex-chinese-fonts/master/english/Serif/TimesNewRomanBoldItalic.ttf
-wget -c https://raw.githubusercontent.com/Haixing-Hu/latex-chinese-fonts/master/chinese/%E4%BB%BF%E5%AE%8B%E4%BD%93/FangSong.ttf
-wget -c https://raw.githubusercontent.com/Haixing-Hu/latex-chinese-fonts/master/chinese/%E5%AE%8B%E4%BD%93/SimSun.ttc
-wget -c https://raw.githubusercontent.com//Haixing-Hu/latex-chinese-fonts/master/chinese/%E9%BB%91%E4%BD%93/SimHei.ttf
-wget -c https://raw.githubusercontent.com//Haixing-Hu/latex-chinese-fonts/master/chinese/%E6%A5%B7%E4%BD%93/Kaiti.ttf
-wget -c https://raw.githubusercontent.com/Halfish/lstm-ctc-ocr/master/fonts/simkai.ttf
+wget -c https://raw.githubusercontent.com/Haixing-Hu/latex-chinese-fonts/refs/heads/master/english/Serif/TimesNewRoman.ttf
+wget -c https://raw.githubusercontent.com/Haixing-Hu/latex-chinese-fonts/refs/heads/master/english/Serif/TimesNewRomanBold.ttf
+wget -c https://raw.githubusercontent.com/Haixing-Hu/latex-chinese-fonts/refs/heads/master/english/Serif/TimesNewRomanItalic.ttf
+wget -c https://raw.githubusercontent.com/Haixing-Hu/latex-chinese-fonts/refs/heads/master/english/Serif/TimesNewRomanBoldItalic.ttf
+wget -c https://raw.githubusercontent.com/Haixing-Hu/latex-chinese-fonts/refs/heads/master/chinese/%E4%BB%BF%E5%AE%8B%E4%BD%93/FangSong.ttf
+wget -c https://raw.githubusercontent.com/Haixing-Hu/latex-chinese-fonts/refs/heads/master/chinese/%E5%AE%8B%E4%BD%93/SimSun.ttc
+wget -c https://raw.githubusercontent.com//Haixing-Hu/latex-chinese-fonts/refs/heads/master/chinese/%E9%BB%91%E4%BD%93/SimHei.ttf
+wget -c https://raw.githubusercontent.com//Haixing-Hu/latex-chinese-fonts/refs/heads/master/chinese/%E6%A5%B7%E4%BD%93/Kaiti.ttf
+wget -c https://raw.githubusercontent.com/Halfish/lstm-ctc-ocr/refs/heads/master/fonts/simkai.ttf
 popd
