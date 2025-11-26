d686e2069f94138d6f2b98a5d612e61a24e3e6f2
Haoran SUN <hrsun@zju.edu.cn>
2025-11-26 14:36:17 +0800

    update times new roman download link

--- scripts/get-fonts.sh
+++ scripts/get-fonts.sh
@@ -1,9 +1,11 @@
 mkdir -p fonts
 pushd fonts
-wget -c https://raw.githubusercontent.com/Haixing-Hu/latex-chinese-fonts/refs/heads/master/english/Serif/TimesNewRoman.ttf
-wget -c https://raw.githubusercontent.com/Haixing-Hu/latex-chinese-fonts/refs/heads/master/english/Serif/TimesNewRomanBold.ttf
-wget -c https://raw.githubusercontent.com/Haixing-Hu/latex-chinese-fonts/refs/heads/master/english/Serif/TimesNewRomanItalic.ttf
-wget -c https://raw.githubusercontent.com/Haixing-Hu/latex-chinese-fonts/refs/heads/master/english/Serif/TimesNewRomanBoldItalic.ttf
+
+wget -c https://github.com/streetsamurai00mi/ttf-ms-win10/raw/refs/heads/build/times.ttf
+wget -c https://github.com/streetsamurai00mi/ttf-ms-win10/raw/refs/heads/build/timesbd.ttf
+wget -c https://github.com/streetsamurai00mi/ttf-ms-win10/raw/refs/heads/build/timesbi.ttf
+wget -c https://github.com/streetsamurai00mi/ttf-ms-win10/raw/refs/heads/build/timesi.ttf
+
 wget -c https://raw.githubusercontent.com/Haixing-Hu/latex-chinese-fonts/refs/heads/master/chinese/%E4%BB%BF%E5%AE%8B%E4%BD%93/FangSong.ttf
 wget -c https://raw.githubusercontent.com/Haixing-Hu/latex-chinese-fonts/refs/heads/master/chinese/%E5%AE%8B%E4%BD%93/SimSun.ttc
 wget -c https://raw.githubusercontent.com//Haixing-Hu/latex-chinese-fonts/refs/heads/master/chinese/%E9%BB%91%E4%BD%93/SimHei.ttf
